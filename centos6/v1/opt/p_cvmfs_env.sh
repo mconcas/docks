bb052c29354d52cd8cc960b69334dd27fed8bedd
Matteo Concas <mconcas@cern.ch>
2015-10-06 17:46:39 +0200

    Updated OCDB ENV configuration.

--- centos6/v1/opt/p_cvmfs_env.sh
+++ centos6/v1/opt/p_cvmfs_env.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-# basic wrapper script to enter in a cvmfs-aware environment
+# wrapper script to enter in an interactive cvmfs-aware environment using parrot_run
 #
 
 export PARROT_ALLOW_SWITCHING_CVMFS_REPOSITORIES=yes
+export CERN_S1="http://cvmfs-stratum-one.cern.ch/cvmfs"
+export DESY_S1="http://grid-cvmfs-one.desy.de:8000/cvmfs"
 export PARROT_CVMFS_REPO="<default-repositories> \
-  alice-ocdb.cern.ch:\
-  url=http://cvmfs-stratum-one.cern.ch/cvmfs/alice-ocdb.cern.ch,\
-  pubkey=$(pwd)/cern-it1.cern.ch.pub"
+ alice-ocdb.cern.ch:url=${CERN_S1}/alice-ocdb.cern.ch,pubkey=/etc/cvmfs/keys/cern.ch/cern-it1.cern.ch.pub \
+ ilc.desy.de:url=${DESY_S1}/ilc.desy.de,pubkey=/etc/cvmfs/keys/desy.de/desy.de.pub"
+
+
+#export PARROT_CVMFS_REPO="<default-repositories> \
+#  alice-ocdb.cern.ch:\
+#  url=http://cvmfs-stratum-one.cern.ch/cvmfs/alice-ocdb.cern.ch,\
+#  pubkey=$(pwd)/cern-it1.cern.ch.pub"
 # export PARROT_OPTIONS="-d cvmfs"
+
 export HTTP_PROXY="http://ca-proxy.cern.ch:3128;DIRECT"
 
 
